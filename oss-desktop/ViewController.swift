d9cfaa02d2f8ab09148acd8dfa4ef421ed2721de
James W. Lane <james.w.lane@mac.com>
2018-03-30 08:35:05 -0400

    Initial Commit

---
+++ oss-desktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  oss-desktop
+//
+//  Created by James Lane on 3/30/18.
+//  Copyright © 2018 James W. Lane. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
